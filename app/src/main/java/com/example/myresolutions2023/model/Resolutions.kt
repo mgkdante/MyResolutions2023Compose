b624c7a1392e9dc4569d0e57eced34a406e3a806
Yesid <mgkdante@gmail.com>
2023-01-14 13:25:42 -0500

    Added new resolution

--- app/src/main/java/com/example/myresolutions2023/model/Resolutions.kt
+++ app/src/main/java/com/example/myresolutions2023/model/Resolutions.kt
@@ -5,12 +5,13 @@ import com.example.myresolutions2023.R
 object Resolutions {
 
     val resolutions = listOf(
-        Resolution(R.string.goal1, R.drawable.run_marathon, R.string.number1),
-        Resolution(R.string.goal2, R.drawable.app_dev, R.string.number2),
-        Resolution(R.string.goal3, R.drawable.android_dev, R.string.number3),
-        Resolution(R.string.goal4, R.drawable.ios_dev, R.string.number4),
-        Resolution(R.string.goal5, R.drawable.live_montreal, R.string.number5),
-        Resolution(R.string.goal6, R.drawable.sell_app, R.string.number6)
+        Resolution(R.string.android_dev, R.drawable.android_dev, R.string.number1),
+        Resolution(R.string.marathon, R.drawable.run_marathon, R.string.number2),
+        Resolution(R.string.professional_dev, R.drawable.app_dev, R.string.number3),
+        Resolution(R.string.hackerrank_modules, R.drawable.hackerrank, R.string.number4),
+        Resolution(R.string.ios_dev, R.drawable.ios_dev, R.string.number5),
+        Resolution(R.string.move_montreal, R.drawable.live_montreal, R.string.number6),
+        Resolution(R.string.sell_app, R.drawable.sell_app, R.string.number7)
     )
 
 }
